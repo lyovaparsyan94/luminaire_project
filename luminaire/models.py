42ea1fd8c8a88b89bba39ef811bd49c95c259619
Lyova Parsyan <parsyan.lyova@gmail.com>
2022-12-26 13:20:15 +0400

    first comm

---
+++ luminaire/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Luminaire(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    content = models.TextField(blank=True, verbose_name="Описание")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',  verbose_name="Фото")
+    # count = models.Count
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано?")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
+
+    class Meta:
+        verbose_name='Светильники'
+        verbose_name_plural='Светильники'
+        ordering = ['id']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категория'
+        ordering = ['id']
